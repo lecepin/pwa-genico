8e5b4a8daa4322592ff08360d83dd5dd95d990fc
唐书 <leping.wlp@alibaba-inc.com>
2019-09-22 18:01:20 +0800

    done

---
+++ precache-manifest.ddd6d3ed128220d465a414cbba43e790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01fe8db04303371b05104ef4746b2e37",
+    "url": "./index.html"
+  },
+  {
+    "revision": "37ea0de76cea966e9f51",
+    "url": "./static/css/2.f3bf1199.chunk.css"
+  },
+  {
+    "revision": "f3717d9163c632151459",
+    "url": "./static/css/main.6328805a.chunk.css"
+  },
+  {
+    "revision": "37ea0de76cea966e9f51",
+    "url": "./static/js/2.4d0b0edc.chunk.js"
+  },
+  {
+    "revision": "f3717d9163c632151459",
+    "url": "./static/js/main.f9d90bac.chunk.js"
+  },
+  {
+    "revision": "6d97820b54a5cd286128",
+    "url": "./static/js/runtime-main.9d31e251.js"
+  }
+]);
